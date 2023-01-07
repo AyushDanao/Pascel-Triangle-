104dd21c775033f007be85346c58cb535fa2659c
AyushDanao <121375207+AyushDanao@users.noreply.github.com>
2023-01-07 13:38:33 +0530

    Add files via upload

---
+++ pascal_traingle.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class pascal_traingle {
+    static void printpascal(int[][] matrix )
+    {
+        for(int i =0;i< matrix.length;i++)
+        {
+            for(int j=0;j<matrix[i].length;j++)
+            {
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    static int[][] pascal(int n)
+    {
+        int[][] ans = new int[n][];
+        for (int i=0;i<n;i++)
+        {
+             ans[i]=new int[i+1];
+            ans[i][0]=ans[i][i]=1;
+            for (int j=1;j<i;j++)
+            {
+//                if(j==0)
+//                {
+//                    ans[i][j]=1;
+//                }
+//                if(j==i)
+//                {
+//                    ans[i][j]=1;
+//                }
+//                else {
+                    ans[i][j]=ans[i-1][j]+ans[i-1][j-1];
+//                }
+            }
+        }
+        return ans ;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter value of n ");
+        int n = sc.nextInt();
+        int[][] ans = pascal(n);
+        printpascal(ans);
+    }
+}
